3e9d1a2dc7a389d38ff76112c5766bb49f19d344
Hlalanathi Mashimbye <hlalianoint@gmail.com>
2024-02-24 23:12:11 +0200

    Update Currency_converter.java

--- Currency_converter.java
+++ Currency_converter.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Currency_converter {
     public static void main(String[] args) {
-    
-    
+        Scanner input = new Scanner(System.in);    
+        System.out.println("Enter the base currency: ");
+        String base_currency = input.nextLine();
+        System.out.println("Enter the target currency: ");
+        String target_currency = input.nextLine();
+        System.out.println("Enter the amount: ");
+        double amount = input.nextDouble();
+        
     }
 
