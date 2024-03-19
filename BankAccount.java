d63f04f1fe7eb2c7e26a0c554d1fb48d3a1890d1
Hlalanathi Mashimbye <hlalianoint@gmail.com>
2024-03-19 17:50:05 +0200

    Add BankAccount class with deposit and withdraw methods

---
+++ BankAccount.java
@@ -0,0 +1,24 @@
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > balance) {
+            System.out.println("Insufficient funds");
+            return false;
+        }
+        balance -= amount;
+        return true;
+    }
+}
